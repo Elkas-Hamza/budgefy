7e631f16f6773871431daa5e86a1b7073ab57188
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2026-03-26 23:17:01 +0100

    components

---
+++ Frontend/src/components/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<template>
+         <h1>hello</h1>
+
+  <div class="ticks"></div>
+  <section id="spacer"></section>
+</template>
