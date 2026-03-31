cc24e89fbbd09f2553874fa2c3673e0e9d7ff72e
Elkas-Hamza <elkasmihamza05@gmail.com>
2026-03-31 21:30:30 +0100

    fix: resolve merge conflict and update template to use router-view

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 <script setup>
 import Home from './components/Home.vue'
 </script>
 <template>
   <Home />
   <h1> welcome to the budgefy app </h1>
-=======
-<template>
-  <router-view />
->>>>>>> d555f2b (feat: add SettingsPage and TransactionsPage components with layout and functionality for user settings and transaction management)
 </template>
