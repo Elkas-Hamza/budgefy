8459553f39b19804e17b0822487840963a0d5143
hyper-ayoub <bouagnaayoub@gmail.com>
2026-04-03 11:31:59 +0100

    added dark an light mode support refactor the landing page

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -4,3 +4,13 @@ import App from './App.vue'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
+
+
+const dark_light = (m) => {
+    const mode = m ?? (localStorage.mode === 'dark' ? 'light' : 'dark')
+    document.documentElement.classList.toggle('dark', mode === 'dark')
+    localStorage.mode = mode
+  }
+dark_light(localStorage.mode)
+window.dark_light = dark_light
+
--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
 @echo off
 REM Start Laravel Backend
-start "Backend Laravel" cmd /k "cd /d Backend && php artisan serve --port=9002"
+start "Backend Laravel" cmd /k "cd /d Backend && php artisan serve --port=8000"
 
 REM Start Vue.js Frontend
-start "Vue.js Frontend" cmd /k "cd /d frontend  && npm run dev -- --port 9000"
+start "Vue.js Frontend" cmd /k "cd /d frontend  && npm run dev -- --port 3000"
 pause
