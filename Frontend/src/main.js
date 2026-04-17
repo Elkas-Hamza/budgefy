375a91aef2b2566cf7ca14024e05d14a36f4bca6
hyper-ayoub <bouagnaayoub@gmail.com>
2026-04-17 10:58:53 +0100

    updates

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
-
+import vuetify from './plugins/vuetify'
+// configuration for vuetify //
 const dark_light = (m) => {
   const savedMode = localStorage.getItem('mode')
   const mode = m ?? (savedMode === 'dark' ? 'light' : 'dark')
@@ -12,6 +13,5 @@ const dark_light = (m) => {
 
 window.dark_light = dark_light
 dark_light(localStorage.getItem('mode'))
-
-createApp(App).use(router).mount('#app')
-
+// updates //
+createApp(App).use(router).use(vuetify).mount('#app')
